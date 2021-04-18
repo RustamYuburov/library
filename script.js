09e466a1bb03e45187c221ecf5f32a09928b36dc
Rustam Yuburov <rt138328@gmail.com>
2021-04-18 14:45:55 +0600

    Updated main.css and index.html

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-const library = [];
+const form = document.querySelector('.input-form');
+
+let library = [];
+
+function Book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+function addBookToLibrary() {
+    const title = document.querySelector('title')
+    console.log(title);
+}
+
+/* 
+    function open form {
+        change scale
+    }
+*/
+
+/* 
+    function create book to shelf from array{
+        foreach book in library makebook
+    }
+*/
+
+/* 
+function submit {
+    take input from form;
+    create book from input;
+    clean input form;
+    pop out form
+}
+*/ 
