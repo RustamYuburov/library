28521a2bc931962986061441822c2d54b900b6c1
Rustam Yuburov <rt138328@gmail.com>
2021-04-19 23:13:23 +0600

    Added fucntion for popup module. Built main app logic

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
+// Getting all necessary DOM nodes
+const module = document.querySelector('.module');
+const addButton = document.querySelector('[data-form-target]');
+const closeButton = document.querySelector('[data-close-button]');
+const overlay = document.querySelector('#overlay');
+const submitButton = document.querySelector('.submit-button');
+const form = document.querySelector('#form');
+const bookshelf = document.querySelector('.bookshelf');
 
-const form = document.querySelector('.form');
-const addButton = document.querySelector('.add-button');
-const closeButton = document.querySelector('.close-btn');
+// Setting EventListeners
+addButton.addEventListener('click', showModule);
+closeButton.addEventListener('click', closeModule);
+submitButton.addEventListener('click', addBookToLibrary)
+overlay.addEventListener('click', () => {
+    const modules = document.querySelectorAll('.module.active');
+    modules.forEach(module => {
+        closeModule(module);
+    })
+})
 
-addButton.addEventListener('click', showForm);
-closeButton.addEventListener('click', closeForm);
-
-
-function showForm() {
-    form.style.display = 'block';
+// Function for open & close module
+function showModule() {
+    module.classList.add('active');
+    overlay.classList.add('active');
     addButton.classList.add('rotated');
 }
 
-function closeForm() {
-    form.style.display = 'none';
+function closeModule() {
+    module.classList.remove('active');
+    overlay.classList.remove('active');
     addButton.classList.remove('rotated');
 }
 
+// Main logic
 let library = [];
 
 function Book(title, author, pages, status) {
@@ -26,17 +41,77 @@ function Book(title, author, pages, status) {
     this.status = status;
 }
 
-/* 
-    function create book to shelf from array{
-        foreach book in library makebook
-    }
-*/
 
-/* 
-function submit {
-    take input from form;
-    create book from input;
-    clean input form;
-    pop out form
+function createNewBook() {
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const status = document.querySelector('#status').value;
+    return new Book(title, author, pages, status);
 }
-*/ 
+
+function addBookToLibrary(e) {
+    e.preventDefault();
+    if (library.length >= 5) return limitAlert();
+    library.push(createNewBook());
+    updateBookshelf();
+    form.reset()
+    closeModule();
+}
+
+function makeBookOnShelf(book, i) {
+    let div = document.createElement('div');
+    div.classList.add('book');
+    div.setAttribute('data-index', `${i}`);
+
+    let bookNode = document.createElement('div');
+    bookNode.classList.add('book-content');
+    
+    let titleNode = document.createElement('h3');
+    titleNode.textContent = `Title: ${book.title}`;
+    
+    let authorNode = document.createElement('h3');
+    authorNode.textContent = `Written by: ${book.author}`;
+    
+    let pagesNode = document.createElement('h3');
+    pagesNode.textContent = `Number of pages: ${book.pages}`;
+    
+    let statusNode = document.createElement('h3');
+    statusNode.classList.add('status-book');
+    statusNode.textContent = `Status: ${book.status}`;
+    
+    let updateButton = document.createElement('button');
+    updateButton.classList.add('update-book');
+    updateButton.textContent = 'Update';
+    
+    let deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-book');
+    deleteButton.textContent = 'Delete';
+    
+    div.appendChild(bookNode);
+    bookNode.appendChild(titleNode);
+    bookNode.appendChild(authorNode);
+    bookNode.appendChild(pagesNode);
+    bookNode.appendChild(statusNode);
+    div.appendChild(updateButton);
+    div.appendChild(deleteButton);
+    bookshelf.appendChild(div);
+}
+
+function updateBookshelf() {
+    bookshelf.innerHTML = ''
+    library.forEach(function (book, i) {
+        makeBookOnShelf(book, i);
+    })
+}
+
+
+// Helper functions
+function limitAlert() {
+    alert('Too much books! Delete some books from library');
+}
+// function saveLocal() {
+//     localStorage.setItem("library", JSON.stringify(library));
+//   }
+
+console.log(library);
